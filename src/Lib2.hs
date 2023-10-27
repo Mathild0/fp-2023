96f820d5762147f72cc7dfa83ae9968e38d7f9ad
evelina-adomaityte <evelina.adomaityte@mif.stud.vu.lt>
2023-10-27 16:22:27 +0300

    show tables

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -7,21 +7,44 @@ module Lib2
   )
 where
 
-import DataFrame (DataFrame)
-import InMemoryTables (TableName)
+import DataFrame (DataFrame(..), Column(..), ColumnType(..), Value(..))
+import InMemoryTables (TableName, database)
+import Data.Char (toLower)
+import Text.Parsec (parse)
+import Data.String (IsString)
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame)]
 
+type ColumnName = String
+
 -- Keep the type, modify constructors
-data ParsedStatement = ParsedStatement
+data ParsedStatement 
+  = ParsedStatement
+  | SelectSumStatement AggregateFuction ColumnName TableName
+  | ShowTables
+  deriving Show
+
+data AggregateFuction 
+  = Sum
+  deriving Show
 
 -- Parses user input into an entity representing a parsed
 -- statement
 parseStatement :: String -> Either ErrorMessage ParsedStatement
-parseStatement _ = Left "Not implemented: parseStatement"
+parseStatement input =
+  let lowercaseInput = map toLower input
+  in case words lowercaseInput of
+    --["select", "sum", "(", column, ")", "from", table] -> Right ()
+    ["show", "tables"] -> Right ShowTables
+    _ -> Left "Invalid statement"
 
--- Executes a parsed statemet. Produces a DataFrame. Uses
+columnName :: Column -> String
+columnName (Column name _) = name
+
+-- Executes a parsed statement. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
 executeStatement :: ParsedStatement -> Either ErrorMessage DataFrame
+executeStatement ShowTables = Right $ DataFrame [Column "Tables" StringType] (map (\(name, _) -> [StringValue name]) database)
+
 executeStatement _ = Left "Not implemented: executeStatement"
