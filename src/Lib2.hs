8a2df9b2c012563849670110dbe74dd1b535b3a2
Mathild0 <118448921+Mathild0@users.noreply.github.com>
2023-11-06 20:21:42 +0200

    fixed bugs and added where and

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -16,12 +16,12 @@ module Lib2
 where
 
 import DataFrame (DataFrame(..), Column(..), ColumnType(..), Value(..), Row)
-import InMemoryTables (TableName, database)
+
 import Lib1 (findTableByName)
 import Data.Char (toLower, isSpace)
-import Data.String (IsString)
-import Data.Maybe (fromMaybe, mapMaybe)
-import Data.List (findIndex, find, isSuffixOf, isPrefixOf, group, elemIndex)
+import Data.List (findIndex, isPrefixOf, isSuffixOf, group, elemIndex)
+import Data.Maybe (fromMaybe, mapMaybe, isJust, catMaybes)
+import InMemoryTables (TableName, database)
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame)]
@@ -32,10 +32,26 @@ data ParsedStatement
   = ParsedStatement
   | SelectSumStatement AggregateFunction ColumnName TableName
   | SelectMinStatement AggregateFunction ColumnName TableName
-  | SelectColumnListStatement [String] TableName 
+  | SelectColumnListStatement [String] TableName
   | ShowTables
   | ShowTable TableName
+  | SelectAndStatement [ParsedCondition] TableName
+  | SelectWhereStatement [ParsedCondition] TableName  -- New constructor for conditions
   deriving (Show, Eq)
+
+
+data ParsedCondition
+  = EqualCondition ColumnName Value
+  | NotEqualCondition ColumnName Value
+  | LessThanCondition ColumnName Value
+  | LessThanOrEqualCondition ColumnName Value
+  | GreaterThanCondition ColumnName Value
+  | GreaterThanOrEqualCondition ColumnName Value
+  deriving (Show, Eq)
+
+
+
+
 
 data AggregateFunction
   = Sum
@@ -48,88 +64,103 @@ instance Eq AggregateFunction where
   Min == Min = True
   _   == _   = False
 
--- Parses user input into an entity representing a parsed
--- statement
+-- Extract table names from the database
+tableNames :: [TableName]
+tableNames = map fst database
+
+-- Define the list of valid command keywords
+commandKeywords :: [String]
+commandKeywords = ["select", "*", "from", "show", "table", "tables"]
+
+-- Combine the command keywords with table names
+names :: [String]
+names = commandKeywords ++ tableNames
+
+-- Modify parseStatement to handle conditions
 parseStatement :: String -> Either ErrorMessage ParsedStatement
 parseStatement input =
   let tokens = words input
-      lowercaseKeywords = ["select", "show", "table", "tables", "from", "min", "sum"]
-      lowercaseToken t = if map toLower t `elem` lowercaseKeywords then map toLower t else t
-      normalizedTokens = map lowercaseToken tokens
+      normalizedTokens = map (map toLower) tokens
   in case normalizedTokens of
     ["show", "tables"] -> Right ShowTables
     ["show", "table", tableName] -> Right (ShowTable tableName)
+    ["select", "*", "from", table] -> Right (SelectColumnListStatement ["*"] table)
     ["select", "sum", "(", column, ")", "from", table] ->
       Right (SelectSumStatement Sum column table)
     ["select", "min", "(", column, ")", "from", table] ->
       Right (SelectMinStatement Min column table)
-    ("select" : rest) ->
-      case break (== "from") rest of
-        (columnPart, "from" : tablePart) ->
-          (if checkColumnList (unwords columnPart) then (do
-          let tableName = unwords tablePart
-          let column = parseColumnList (unwords columnPart)
-          Right (SelectColumnListStatement column tableName)) else Left "Invalid statement")
-        _ -> Left "Invalid statement"
+    ["select", "*", "from", table, "where", conditionStr] ->
+      case parseConditions conditionStr of
+        Just conditions -> Right (SelectWhereStatement conditions table)
+        Nothing -> Left "Invalid condition"
+    ["select", "*", "from", table, "where", column, "=", value] -> -- Add this case
+      case readValue value of
+        Just v -> Right (SelectWhereStatement [EqualCondition column v] table)
+        Nothing -> Left "Invalid value"
+    ("select" : "*" : "from" : table : "where" : rest) ->
+      case parseConditions (unwords rest) of
+        Just conditions -> Right (SelectWhereStatement conditions table)
+        Nothing -> Left "Invalid condition"
+    ("select" : "*" : "from" : table : "and" : rest) ->
+      case parseConditions (unwords rest) of
+        Just conditions -> Right (SelectAndStatement conditions table)
+        Nothing -> Left "Invalid condition"
     _ -> Left "Invalid statement"
 
 columnName :: Column -> String
 columnName (Column name _) = name
 
 executeStatement :: ParsedStatement -> Either ErrorMessage DataFrame
-
-executeStatement ShowTables = 
+executeStatement ShowTables =
   Right $ DataFrame [Column "Tables" StringType] (map (\(name, _) -> [StringValue name]) database)
 
-executeStatement (ShowTable tableName) = 
+executeStatement (ShowTable tableName) =
   case findTableByName database tableName of
     Just table -> Right table
     Nothing -> Left "Table not found"
 
 executeStatement (SelectSumStatement Sum column tableName) =
   case findTableByName database tableName of
-    Just (DataFrame columns rows) -> 
-      
+    Just (DataFrame columns rows) ->
       case findColumnIndex column columns of
-        Just idx -> do
-
+        Just idx ->
           let values = extractColumnValues column columns rows
-
-          case calculateSum values of
+          in case calculateSum values of
             Just sumValue -> Right $ DataFrame [Column "Sum" IntegerType] [[IntegerValue sumValue]]
             Nothing -> Left "Column not found or an invalid sum statement"
-            
         Nothing -> Left "Column not found or an invalid sum statement"
-    
     Nothing -> Left "Table not found"
 
 executeStatement (SelectMinStatement Min columnName tableName) =
-  case lookup tableName database of
-    Just (DataFrame columns rows) -> do
-
+  case findTableByName database tableName of
+    Just (DataFrame columns rows) ->
       let columnIndex = findColumnIndex columnName columns
-
-      case columnIndex of
-        Just idx -> do
-          -- Extract the column data
+      in case columnIndex of
+        Just idx ->
           let columnData = [rowData !! idx | rowData <- rows]
-
-          -- Calculate the minimum value
-          let minValue = calculateMinimum columnData
-
-          -- Create a result DataFrame
-          Right $ DataFrame [Column "Result" (getColumnType minValue)] [[minValue]]
-
+              minValue = calculateMinimum columnData
+          in Right $ DataFrame [Column "Result" (getColumnType minValue)] [[minValue]]
         Nothing -> Left "Column not found"
+    Nothing -> Left "Table not found"
 
+executeStatement (SelectAndStatement conditions tableName) =
+  case findTableByName database tableName of
+    Just (DataFrame columns rows) ->
+      let matchingRows = filter (matchesAllConditions conditions columns) rows
+      in Right (DataFrame columns matchingRows)
     Nothing -> Left "Table not found"
 
 executeStatement (SelectColumnListStatement columns tableName) =
-  case lookup tableName database of
-    Just table ->
-      case selectColumns columns table of
-        Left e -> Left e
-        Right selectedDf -> Right selectedDf
+  case findTableByName database tableName of
+    Just table -> selectColumns columns table
+    Nothing -> Left "Table not found"
+
+-- Implement the new SelectWhereStatement in executeStatement
+executeStatement (SelectWhereStatement conditions tableName) =
+  case findTableByName database tableName of
+    Just (DataFrame columns rows) ->
+      let matchingRows = filter (matchesAllConditions conditions columns) rows
+      in Right (DataFrame columns matchingRows)
     Nothing -> Left "Table not found"
 
 executeStatement _ = Left "Not implemented: executeStatement"
@@ -160,7 +191,7 @@ minVal _ _ = NullValue
 -- Helper function to find the index of a column by name
 findColumnIndex :: ColumnName -> [Column] -> Maybe Int
 findColumnIndex columnName =
-  findIndex (\ (Column name _) -> name == columnName) 
+  findIndex (\ (Column name _) -> name == columnName)
 
 -- Helper function to determine the column type based on a Value
 getColumnType :: Value -> ColumnType
@@ -169,7 +200,7 @@ getColumnType value =
     IntegerValue _ -> IntegerType
     BoolValue _    -> BoolType
     StringValue _  -> StringType
-    _              -> StringType 
+    _              -> StringType
 
 strip :: String -> String
 strip = f . f
@@ -190,12 +221,21 @@ checkColumnList input
 
 selectColumns :: [String] -> DataFrame -> Either ErrorMessage DataFrame
 selectColumns columns (DataFrame dfColumns dfRows) =
-  let columnIndices = getAllIndexes columns dfColumns
-  in if null columnIndices then Left "No columns were found"
-     else
-        let selectedColumns = map (\x -> dfColumns !! x) columnIndices
-            selectedRows = map (selectCells columnIndices) dfRows
-        in Right (DataFrame selectedColumns selectedRows)
+  if null columns || all (== "*") columns
+    then Right (DataFrame dfColumns dfRows)
+    else selectColumns' columns (DataFrame dfColumns dfRows)
+
+selectColumns' :: [String] -> DataFrame -> Either ErrorMessage DataFrame
+selectColumns' requestedColumns (DataFrame dfColumns dfRows) =
+  let columnIndices = getAllIndexes requestedColumns dfColumns
+  in if null columnIndices
+    then Left "No columns were found"
+    else
+      let selectedColumns = map (\x -> dfColumns !! x) columnIndices
+          selectedRows = map (selectCells columnIndices) dfRows
+      in Right (DataFrame selectedColumns selectedRows)
+
+
 
 getAllIndexes :: [String] -> [Column] -> [Int]
 getAllIndexes requestedColumns dfColumns =
@@ -218,3 +258,88 @@ calculateSum values = case traverse extractIntegerValue values of
 extractIntegerValue :: Value -> Maybe Integer
 extractIntegerValue (IntegerValue i) = Just i
 extractIntegerValue _ = Nothing
+
+parseConditions :: String -> Maybe [ParsedCondition]
+parseConditions conditionStr =
+  let conditionTokens = words conditionStr
+      parsedConditions = map parseCondition conditionTokens
+  in if all isJust parsedConditions
+     then Just (catMaybes parsedConditions)
+     else Nothing
+
+parseCondition :: String -> Maybe ParsedCondition
+parseCondition conditionToken
+  | "==" `isSuffixOf` conditionToken = -- Handle "=="
+      let (column, valueStr) = break (== '=') conditionToken
+          value = readValue (drop 2 valueStr) -- drop the '==' part
+      in case value of
+        Just val -> Just (EqualCondition column val)
+        Nothing -> Nothing
+  | "!=" `isSuffixOf` conditionToken =
+      let (column, valueStr) = break (== '!') conditionToken
+          value = readValue (drop 2 valueStr) -- drop the '!=' part
+      in case value of
+        Just val -> Just (NotEqualCondition column val)
+        Nothing -> Nothing
+  | "<" `isSuffixOf` conditionToken =
+      let (column, valueStr) = break (== '<') conditionToken
+          value = readValue (drop 1 valueStr) -- drop the less than sign
+      in case value of
+        Just val -> Just (LessThanCondition column val)
+        Nothing -> Nothing
+  | "<=" `isSuffixOf` conditionToken =
+      let (column, valueStr) = break (== '=') conditionToken
+          value = readValue (drop 2 valueStr) -- drop the '<=' part
+      in case value of
+        Just val -> Just (LessThanOrEqualCondition column val)
+        Nothing -> Nothing
+  | ">" `isSuffixOf` conditionToken =
+      let (column, valueStr) = break (== '>') conditionToken
+          value = readValue (drop 1 valueStr) -- drop the greater than sign
+      in case value of
+        Just val -> Just (GreaterThanCondition column val)
+        Nothing -> Nothing
+  | ">=" `isSuffixOf` conditionToken =
+      let (column, valueStr) = break (== '=') conditionToken
+          value = readValue (drop 2 valueStr) -- drop the '>=' part
+      in case value of
+        Just val -> Just (GreaterThanOrEqualCondition column val)
+        Nothing -> Nothing
+  | otherwise = Nothing
+
+
+-- Helper function to parse an IntegerValue
+readValue :: String -> Maybe Value
+readValue str = case reads str of
+  [(val, "")] -> Just (IntegerValue val)
+  _           -> Nothing
+
+-- to check if a row matches all conditions
+matchesAllConditions :: [ParsedCondition] -> [Column] -> Row -> Bool
+matchesAllConditions conditions columns row =
+  all (\condition -> matchesCondition condition columns row) conditions
+
+matchesCondition :: ParsedCondition -> [Column] -> Row -> Bool
+matchesCondition (EqualCondition colName value) columns row =
+  case findColumnIndex colName columns of
+    Just idx -> row !! idx == value
+    Nothing  -> False
+
+-- Implementation of the whereAND function:
+whereAND :: String -> Either ErrorMessage DataFrame
+whereAND input =
+  case parseStatement input of
+    Right (SelectAndStatement conditions tableName) -> -- Handle SelectAndStatement
+      case findTableByName database tableName of
+        Just (DataFrame columns rows) ->
+          let matchingRows = filter (matchesAllConditions conditions columns) rows
+          in Right (DataFrame columns matchingRows)
+        Nothing -> Left "Table not found"
+    Right (SelectWhereStatement conditions tableName) -> -- Handle SelectWhereStatement
+      case findTableByName database tableName of
+        Just (DataFrame columns rows) ->
+          let matchingRows = filter (matchesAllConditions conditions columns) rows
+          in Right (DataFrame columns matchingRows)
+        Nothing -> Left "Table not found"
+    _ -> Left "Invalid statement"
+
